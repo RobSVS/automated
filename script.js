d13fcfc6419c966c2bc8cfdeb0b52e0004ec92eb
Robin Winroth <robin.winroth@svenskaspel.se>
2022-01-20 13:21:05 +0100

    dckr jnks file

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 var exec = require('child_process').exec;
 
-var cmd = exec("npm --version", function(err, stdout, stderr) {
+var cmd = exec("npm install", function(err, stdout, stderr) {
     if (err) {
       // handle error
+      console.log(err);
     }
     console.log(stdout);
+    cmd2;
 });
 
+var cmd2 = exec("npm run cy:test", function(err, stdout, stderr) {
+    if (err) {
+      // handle error
+      console.log(err);
+    }
+    console.log(stdout);    
+});
 
 cmd;
