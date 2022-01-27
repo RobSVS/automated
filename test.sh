9d0c8b3097f52b7433851dd01ef94ca9deff593f
Robin Winroth <robin.winroth@svenskaspel.se>
2022-01-27 11:56:50 +0100

    new file changes

--- test.sh
+++ test.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
-set e+x
 
+set -f
 LOCAL_NAME=automated
 
-echo "Building $LOCAL_NAME"
-docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t $LOCAL_NAME .
+echo "Running tests against $LOCAL_NAME"
+echo "Running as a non-root user 'appuser' mapped to host user"
+
+set -x
+
+spec=$1
+shift 1
+args="$@"
+
+docker run -i --rm -v "$(pwd)"/cypress:/test/cypress -w /test --name "office-test-${BUILD_NUMBER}" --user appuser $LOCAL_NAME \
+--config video=false --spec ${spec} ${args}
+return_code=$?
+set +x
+exit $return_code
